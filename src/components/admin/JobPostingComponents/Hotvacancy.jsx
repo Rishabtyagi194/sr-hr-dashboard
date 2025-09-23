939cfb36aa748c44fbf7b5e4ca958319fe0838c8
“Mukti” <muktijindal5@gmail.com>
2025-09-23 23:14:52 +0530

    form created and integrated api

--- src/components/admin/JobPostingComponents/Hotvacancy.jsx
+++ src/components/admin/JobPostingComponents/Hotvacancy.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import JobPostForm from './HotJobPostForm'
 
 export const Hotvacancy = () => {
   return (
-    <div>Hotvacancy</div>
+    <div>
+        <JobPostForm />
+    </div>
   )
 }
